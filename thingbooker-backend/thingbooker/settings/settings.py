07792b29c29ac632e3afa4d170bd8e97dafe2d1e
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2023-12-23 22:22:47 +0100

    config: use decouple in settings

--- thingbooker-backend/thingbooker/settings/settings.py
+++ thingbooker-backend/thingbooker/settings/settings.py
@@ -1,21 +1,19 @@
 """This module contains the settings for thingbooker-backend"""
 
-from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+from decouple import config
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wk9&=rl0ofn)hhx+cmu-1n8q903@26!m+_#ofyfeqx^2_%exg8"
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+ALLOWED_HOSTS: list[str] = ["*"]
 
-ALLOWED_HOSTS: list[str] = []
+CLIENT_URL = config("FRONTEND_URL")
 
 
 # Application definition
@@ -65,8 +63,12 @@ WSGI_APPLICATION = "thingbooker.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
 
@@ -97,7 +99,7 @@ LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
 
-USE_I18N = True
+USE_I18N = False
 
 USE_TZ = True
 
