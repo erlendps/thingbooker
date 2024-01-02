7ffe5fa6d8b23a76154c9ce7140440c1147c35ce
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2024-01-02 23:44:55 +0100

    feat: add util function for hasing strings

--- thingbooker-backend/thingbooker/utils.py
+++ thingbooker-backend/thingbooker/utils.py
@@ -1,3 +1,4 @@
+import hashlib
 from base64 import urlsafe_b64encode
 from os import urandom
 
@@ -10,3 +11,11 @@ def create_token(length: int = settings.TOKEN_BYTE_LENGTH):
     random = urandom(length)
     token = urlsafe_b64encode(random)[:-1]  # remove = suffix of base64
     return token.decode("utf-8")
+
+
+def hash_token(token: str):
+    """Hashes the given string using sha256"""
+
+    sha256 = hashlib.sha256()
+    sha256.update(bytes(token, "utf-8"))
+    return sha256.hexdigest()
