811da3464fd0f87c8b807818078dea65b10a8689
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2024-01-07 18:00:47 +0100

    refactor: override default allauth email templates

--- thingbooker-backend/thingbooker/settings/settings.py
+++ thingbooker-backend/thingbooker/settings/settings.py
@@ -78,7 +78,10 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "templates", "allauth"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
