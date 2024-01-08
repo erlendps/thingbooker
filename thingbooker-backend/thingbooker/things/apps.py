714f97ae4048167f94517988a26e264d6eb4eb53
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2024-01-08 14:36:14 +0100

    chore: add things app skeleton

---
+++ thingbooker-backend/thingbooker/things/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ThingsConfig(AppConfig):
+    """Config for things app"""
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "thingbooker.things"
