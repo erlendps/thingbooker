ff0c51af712e55948f9a4b3103ad5f00975cbe52
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2024-03-10 16:15:04 +0100

    frontend: add component for user avatar

---
+++ thingbooker-frontend/src/lib/components/UserAvatar.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { ShortUserType } from '$lib/types';
+
+  export let user: ShortUserType;
+  export let tailwindHeight = 'h-12';
+</script>
+
+{#key user}
+  <img
+    src={user.avatar}
+    alt="Avatar til {user.first_name}"
+    class="border-whisper border-2 rounded-full bg-marshland {tailwindHeight}"
+  />
+{/key}
