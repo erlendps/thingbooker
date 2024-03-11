9cb382cf1492f2bd898208282f3b2127f86751fd
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2024-03-11 18:45:09 +0100

    frontend: add tailwind color maps

---
+++ thingbooker-frontend/src/lib/utils/style.ts
@@ -0,0 +1,47 @@
+export const textColorMap = {
+  whisper: 'text-whisper',
+  'willow-grove': 'text-willow-grove',
+  marshland: 'text-marshland',
+  tropaz: 'text-tropaz',
+  'solid-pink': 'text-solid-pink'
+};
+
+export const hoverTextColorMap = {
+  whisper: 'hover:text-whisper-dark',
+  'willow-grove': 'hover:text-willow-grove-dark',
+  marshland: 'hover:text-marshland-dark',
+  tropaz: 'hover:text-tropaz-dark',
+  'solid-pink': 'hover:text-solid-pink-dark'
+};
+
+export const fillColorMap = {
+  whisper: 'bg-whisper',
+  'willow-grove': 'bg-willow-grove',
+  marshland: 'bg-marshland',
+  tropaz: 'bg-tropaz',
+  'solid-pink': 'bg-solid-pink'
+};
+
+export const hoverFillColorMap = {
+  whisper: 'hover:bg-whisper-dark',
+  'willow-grove': 'hover:bg-willow-grove-dark',
+  marshland: 'hover:bg-marshland-dark',
+  tropaz: 'hover:bg-tropaz-dark',
+  'solid-pink': 'hover:bg-solid-pink-dark'
+};
+
+export const outlineColorMap = {
+  whisper: 'outline-whisper',
+  'willow-grove': 'outline-willow-grove',
+  marshland: 'outline-marshland',
+  tropaz: 'outline-tropaz',
+  'solid-pink': 'outline-solid-pink'
+};
+
+export const focusOutlineColorMap = {
+  whisper: 'focus-visible:outline-whisper',
+  'willow-grove': 'focus-visible:outline-willow-grove',
+  marshland: 'focus-visible:outline-marshland',
+  tropaz: 'focus-visible:outline-tropaz',
+  'solid-pink': 'focus-visible:outline-solid-pink'
+};
