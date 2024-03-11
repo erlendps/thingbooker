d1987953ddd1c30e18560b9e18cc3897c0ef063f
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2024-03-11 18:44:35 +0100

    frontend: dont use env variable for unprotected pages

--- thingbooker-frontend/src/lib/utils/constants.ts
+++ thingbooker-frontend/src/lib/utils/constants.ts
@@ -1,4 +1,3 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-const unprotectedPages: string = import.meta.env.VITE_UNPROTECTED_PAGES;
-export const UNPROTECTED_PAGES = unprotectedPages.split(',');
+export const UNPROTECTED_PAGES = ['/(unauthenticated)', '/(unauthenticated)/auth/login'];
 export const BASE_LOGGEDIN_URL = '/tb';
