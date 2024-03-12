85b04a15133d9eb0504953830558e6c39bffb1dc
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2024-03-12 18:31:01 +0100

    frontend: implement api for group management

--- thingbooker-frontend/src/lib/utils/backend.ts
+++ thingbooker-frontend/src/lib/utils/backend.ts
@@ -13,6 +13,7 @@ const createFormData = ({ ...fields }) => {
 const AUTH_URL = BACKEND_URL + 'auth/';
 
 const auth = {
+  AUTH_URL,
   /**
    * Tries to log in using the credentials provided.
    *
@@ -177,8 +178,131 @@ const auth = {
   }
 };
 
+const ACCOUNTS_URL = BACKEND_URL + 'accounts/';
+const accounts = {
+  ACCOUNTS_URL,
+  getAllKnownUsers: async (short: boolean = false) => {
+    return await fetch(ACCOUNTS_URL + `?short=${short}`);
+  },
+  getUserById: async (userId: string, short: boolean = false) => {
+    return await fetch(ACCOUNTS_URL + `${userId}/?short=${short}`);
+  }
+};
+
+const GROUPS_URL = BACKEND_URL + 'groups/';
+const TOKENS_URL = BACKEND_URL + 'invite-tokens';
+const groups = {
+  GROUPS_URL,
+  TOKENS_URL,
+  /**
+   * Fetches all the groups the user knows.
+   *
+   * @returns response from backend
+   */
+  getAllKnownGroups: async () => {
+    return await fetch(GROUPS_URL);
+  },
+  /**
+   * Fetches a group by id. If the group is not 'known' to the user, 404 is returned
+   *
+   * @param groupId
+   * @returns response from backend
+   */
+  getGroupById: async (groupId: string) => {
+    return await fetch(GROUPS_URL + `${groupId}/`);
+  },
+  /**
+   * Deletes the given group. Only the owner can do this.
+   *
+   * @param groupId
+   * @returns response from backend
+   */
+  deleteGroup: async (groupId: string) => {
+    return await fetch(GROUPS_URL + `${groupId}/`, { method: 'DELETE' });
+  },
+  /**
+   * Creates a new group with owner set as the current user.
+   *
+   * @param name
+   * @param groupPicture
+   * @returns response from backend
+   */
+  createGroup: async (name: string, groupPicture?: File) => {
+    const data = createFormData({ name, group_picture: groupPicture });
+
+    return await fetch(GROUPS_URL, { method: 'POST', body: data });
+  },
+  /**
+   * Updates the group with corresponding groupId. Only the owner can do this
+   *
+   * @param groupId
+   * @param param1
+   * @returns response from backend
+   */
+  updateGroup: async (
+    groupId: string,
+    { name, groupPicture }: { name?: string; groupPicture?: File }
+  ) => {
+    const data = createFormData({ name, group_picture: groupPicture });
+
+    return await fetch(GROUPS_URL + `${groupId}/`, { method: 'PATCH', body: data });
+  },
+  /**
+   * Invites a thingbooker user to the group, if the user exists.
+   *
+   * @param groupId
+   * @param email email of the user being invited
+   * @returns response from backend
+   */
+  inviteMemberToGroup: async (groupId: string, email: string) => {
+    const data = createFormData({ email });
+
+    return await fetch(GROUPS_URL + `${groupId}/invite_member/`, { method: 'POST', body: data });
+  },
+  /**
+   * Accepts a group invite.
+   *
+   * @param token
+   * @returns response from backend
+   */
+  acceptGroupInvite: async (token: string) => {
+    return await fetch(TOKENS_URL + `accept-invite/${token}/`, { method: 'POST' });
+  },
+  /**
+   * Fetches a list of all invite tokens. Only a admin can do this.
+   *
+   * @returns response from backend
+   */
+  getAllInviteTokens: async () => {
+    return await fetch(TOKENS_URL);
+  },
+  /**
+   * Fetches the token with the given tokenId. Only an admin can do this.
+   *
+   * @param tokenId
+   * @returns response from backend
+   */
+  getInviteToken: async (tokenId: string) => {
+    return await fetch(TOKENS_URL + `${tokenId}/`);
+  }
+};
+
+const THINGS_URL = BACKEND_URL + 'things/';
+const things = { THINGS_URL };
+
+const BOOKINGS_URL = BACKEND_URL + 'bookings/';
+const bookings = { BOOKINGS_URL };
+
+const RULES_URL = BACKEND_URL + 'rules/';
+const rules = { RULES_URL };
+
 const api = {
-  auth
+  accounts,
+  auth,
+  bookings,
+  groups,
+  rules,
+  things
 };
 
 export default api;
