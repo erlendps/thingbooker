95dbcf40d9799224d9b04cd00156002171f0a396
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2024-03-27 13:22:00 +0100

    backend: register accept thing invite viewset url

--- thingbooker-backend/thingbooker/users/urls.py
+++ thingbooker-backend/thingbooker/users/urls.py
@@ -1,11 +1,15 @@
 from rest_framework.routers import SimpleRouter
 
-from thingbooker.users.views import GroupInviteTokenViewSet, GroupViewSet, UserViewSet
+from thingbooker.users.views import (
+    GroupInviteTokenViewSet,
+    GroupViewSet,
+    ThingInviteTokenViewSet,
+    UserViewSet,
+)
 
 router = SimpleRouter()
 
 router.register("accounts", UserViewSet, basename="thingbookeruser")
 router.register("groups", GroupViewSet, basename="thingbookergroup")
-router.register(
-    "group-invite-tokens", GroupInviteTokenViewSet, basename="thingbooker-group-invite-token"
-)
+router.register("group-invite-tokens", GroupInviteTokenViewSet, basename="acceptgroupinvitetoken")
+router.register("thing-invite-tokens", ThingInviteTokenViewSet, basename="acceptthinginvitetoken")
